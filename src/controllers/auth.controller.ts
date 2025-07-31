cb3d2fbf6a87632b280d443c3fdbd74c532616af
Drynaa <drynaacode@gmail.com>
2025-07-31 22:43:34 +0200

    fix: Ajustement de la politique sameSite des cookies pour le cross-domain

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -115,7 +115,7 @@ export async function login(req: Request, res: Response) {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "lax",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax", // 👈 MODIFIEZ CETTE LIGNE
       maxAge: 60 * 60 * 1000, // 1h
     });
 
@@ -123,7 +123,7 @@ export async function login(req: Request, res: Response) {
     res.cookie("refreshToken", refreshToken, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "lax",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax", // 👈 MODIFIEZ CETTE LIGNE
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 jours
     });
 
